8e590bc8c4d50535f6b161d8266566734b6e6606
Howard Liu <howardliu@Howards-Mac-Studio.local>
2025-10-20 09:37:01 -0400

    main_repl

---
+++ R/main_repl.R
@@ -0,0 +1,68 @@
+rm(list=ls(all=TRUE))
+cat("\014")
+
+library(tidyverse)
+library(fixest)
+library(glmmTMB)
+library(texreg)
+
+
+# Load data -----------------------------------------
+load("data/panelDat.rda") # 46 vars
+
+# model 1
+model_vio_feols <- feols(policeUC_arrest_bin 
+                         ~ cl_quart_sw + cl_quarter + policeUC_count_bin + policeUni_car_count_bin + pArrest_count_sw + violence_escalation 
+                         | CACODE,
+                         data=panel_dat, panel.id = ~ CACODE) 
+
+# model 2
+model_vio_feols.t <- feols(policeUC_arrest_bin
+                           ~ cl_quart_sw + cl_quarter + policeUC_count_bin + policeUni_car_count_bin + pArrest_count_sw + violence_escalation  + time +time2 + time3 
+                           | CACODE, 
+                           data=panel_dat, panel.id = ~ CACODE) 
+
+# model 3
+model_vio_feols_iv <- feols(policeUC_arrest_bin 
+                            ~ policeUC_count_bin + policeUni_car_count_bin + pArrest_count_sw + violence_escalation 
+                            | CACODE | cl_quart_sw + cl_quarter ~ weekend + day,
+                            data=panel_dat, panel.id = ~ CACODE)
+
+# model 4
+model_vio_feols_iv.t <- feols(policeUC_arrest_bin ~ policeUC_count_bin + policeUni_car_count_bin + pArrest_count_sw + violence_escalation + time +time2 + time3 
+                              | CACODE | cl_quart_sw + cl_quarter ~ weekend + day ,
+                              data=panel_dat, panel.id = ~ CACODE)
+
+# model 5
+re_vio.mod <- glmmTMB(policeUC_arrest_bin ~ cl_quart_sw + cl_quarter + policeUC_count_bin + policeUni_car_count_bin + pArrest_count_sw + violence_escalation + indoorSp_protests + legCo  + (1 | CACODE), data = panel_dat, family="gaussian") 
+
+# model 6
+re_vio.mod.t <- glmmTMB(policeUC_arrest_bin ~ cl_quart_sw + cl_quarter + policeUC_count_bin + policeUni_car_count_bin + pArrest_count_sw + violence_escalation + indoorSp_protests + legCo + time +time2 + time3 + (1 | CACODE), data = panel_dat, family="gaussian")
+
+
+# main result (Table 1)--------------------------------
+idvs = c("Closeness to protest zone",  "Protest zone", 
+         "Undercover Police", "Uniformed Police", "Arrests nearby", "Protest violence",
+          "time", "time2", "time3",
+         "Predicted closeness to protest zone","Predicted protest zone", 
+          "Intercept", 
+          "Indoor spaces", "LegCo"
+)
+
+screenreg(list(model_vio_feols, model_vio_feols.t,
+               model_vio_feols_iv,model_vio_feols_iv.t,
+               re_vio.mod, re_vio.mod.t),
+          include.variance=F, include.intercept = F,
+          include.rsquared = F, include.loglik = F,
+          include.aic = F, include.adjr = F,
+          digits = 4,
+          stars = c(0.01, 0.05, 0.10),
+          custom.model.names = c(
+            "OLS", "OLS",
+            "2SLS","2SLS",
+            "OLS", "OLS"),
+          custom.coef.names = idvs,
+          custom.gof.rows = list(Model = c("FE", "FE", "FE", "FE", "RE", "RE"), TimePoly = c("N", "Y", "N", "Y","N", "Y")),
+          omit.coef = "(time|time2|time3|(Intercept))"
+          ) 
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# undercoverPolicing
+# Covert Assignments: Undercover Infiltration and the Repression of Protests
+
+Authors
+---
+- Howard Liu
+- Benjamin J. Radford
+
+
+Replication Instructions
+---
+This folder contains all materials needed to replicate our results. You can find the replication codes in the *R* folder. Results can be replicated by either the *.R* code or the *.qmd* code. *.R* code will show all results in R while *.qmd* code will produce the results in a clean pdf. We recommend opening the *undercoverPolicing.Rproj* and run the code because doing so you won't be needing to set working directories. 
